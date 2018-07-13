396fbf171f24562e55e7d9bd5e51d8791df067b4
Jakub Dubański <jakub.dubanski@gmail.com>
2018-07-13 20:50:20 +0200

    [Algorithms] BubbleSort with end control 1.0

---
+++ src/main/java/com/publicclass/algorithms/BubbleSortWithEndControl.java
@@ -0,0 +1,45 @@
+package com.publicclass.algorithms;
+
+import java.util.Arrays;
+
+// Kubanino @ https://public-class.com/
+public class BubbleSortWithEndControl
+{
+    private static void swap(int[] array, int index1, int index2)
+    {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+    private static void sort(int[] array)
+    {
+        for (int i = 0; i < array.length - 1; i++)
+        {
+            boolean somethingWasSwapped = false;
+            for (int j = 0; j < array.length - 1 - i; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    swap(array, j, j + 1);
+                    somethingWasSwapped = true;
+                }
+                if (j == array.length - 2 - i)
+                {
+                    if (!somethingWasSwapped)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int[] input = {7777, 1, 2, 5, 3, 2, 6, 555, 7, 2, 3, 123, 54, 324, 17};
+        sort(input);
+
+        System.out.println(Arrays.toString(input));
+    }
+}
