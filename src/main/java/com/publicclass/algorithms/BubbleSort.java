0b4f6184b972894289793031a5aa6e04ea3b04e4
Jakub Dubański <jakub.dubanski@gmail.com>
2018-07-13 20:38:11 +0200

    [Algorithms] Simple BubbleSort 1.0.

---
+++ src/main/java/com/publicclass/algorithms/BubbleSort.java
@@ -0,0 +1,35 @@
+package com.publicclass.algorithms;
+
+import java.util.Arrays;
+
+public class BubbleSort
+{
+    private static void swap(int[] array, int index1, int index2)
+    {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+    private static void sort(int[] array)
+    {
+        for (int i = 0; i < array.length - 1; i++)
+        {
+            for (int j = 0; j < array.length - 1 - i; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    swap(array, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int[] input = {7777, 1, 2, 5, 3, 2, 6, 555, 7, 2, 3, 123, 54, 324, 17};
+        sort(input);
+
+        System.out.println(Arrays.toString(input));
+    }
+}
