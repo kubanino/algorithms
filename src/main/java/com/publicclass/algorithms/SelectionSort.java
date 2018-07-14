c0edb2643fa372633164c8de75ec465899cd5917
Jakub Dubański <jakub.dubanski@gmail.com>
2018-07-14 11:54:09 +0200

    [Algorithms] SelectionSort implementation 1.0

---
+++ src/main/java/com/publicclass/algorithms/SelectionSort.java
@@ -0,0 +1,38 @@
+package com.publicclass.algorithms;
+
+import java.util.Arrays;
+
+// Kubanino @ https://public-class.com/
+public class SelectionSort
+{
+    private static void swap(int[] array, int index1, int index2)
+    {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+    private static void sort(int[] array)
+    {
+        for (int i = 0; i < array.length; i++)
+        {
+            int indexOfSmallerElement = i;
+            for (int j = i; j < array.length; j++)
+            {
+                indexOfSmallerElement = (array[j] < array[indexOfSmallerElement]) ? j : indexOfSmallerElement;
+            }
+            if (indexOfSmallerElement != i)
+            {
+                swap(array, indexOfSmallerElement, i);
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int[] input = {7777, 1, 2, 5, 3, 2, 6, 555, 7, 2, 3, 123, 54, 324, 17};
+        sort(input);
+
+        System.out.println(Arrays.toString(input));
+    }
+}
